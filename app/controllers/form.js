b52707e4e3cf9aadf80c3b80259e0d318db21a71
mateo <soporte@argul.com.ar>
2023-03-02 10:43:08 -0300

    se agregan al formulario funcion update y funcion delete

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -1,11 +1,12 @@
-const formModel=require('../models/form')
-const { httpError } = require("../helpers/handleError")
+const formModel = require('../models/form')
+const { httpError } = require("../helpers/handleError");
+const { findByIdAndDelete } = require('../models/form');
 
-const createForm= async (req,res)=>{
+const createForm = async (req, res) => {
     try {
         var today = new Date();
         var now = today.toLocaleString();
-    const {
+        const {
             //informacion de la orden de produccion
             nombre_formulario,
             numero_operacion,
@@ -51,8 +52,8 @@ const createForm= async (req,res)=>{
             tipo_expulsion,
             //Cavidades habilitadas en produccion
             cavidades,
-        observaciones
-            }=req.body
+            observaciones
+        } = req.body
 
 
         const formDetail = await formModel.create({
@@ -71,20 +72,20 @@ const createForm= async (req,res)=>{
             tiempo_ciclo,
             aditivo,
             registrado_por,
-        //Dispositivos utilizados
+            //Dispositivos utilizados
             pico_de_inyectora,
             microgel_0,
             microgel_1,
             regoplas,
             robot,
             otro,
-        //Temperaturas del proceso
+            //Temperaturas del proceso
             temp_plato_fijo,
             temp_plato_movil,
             temp_masa_fundida,
             tablero_colada_caliente,
             temp_horno,
-        //Parametros del proceso
+            //Parametros del proceso
             tpo_ciclo_optimo,
             tpo_ciclo_actual,
             tpo_inyeccion,
@@ -99,61 +100,183 @@ const createForm= async (req,res)=>{
             presion_empaque,
             tpo_retencion,
             tipo_expulsion,
-        //Cavidades habilitadas en produccion
+            //Cavidades habilitadas en produccion
             cavidades,
             observaciones,
             hora: now
         })
         // res.send(formDetail)
         return res.status(200).send("Formulario enviado con exito!")
-    }catch(e){
-        res.send({data:{},
-        status:false}),
-        httpError(res,e)
+    } catch (e) {
+        res.send({
+            data: {},
+            status: false
+        }),
+            httpError(res, e)
     }
 
 }
 
-const getFilter= async (req,res)=>{
-    try{
-    let match={}
-    const keyword=req.query.keyword
-    
-    if (req.query.keyword){
-        match.$or=[
-            {materia_prima0:new RegExp(req.query.keyword,"i")},
-            {materia_prima1:new RegExp(req.query.keyword,"i")},
-            {materia_prima2:new RegExp(req.query.keyword,"i")},
-            {materia_prima3:new RegExp(req.query.keyword,"i")},
-            {materia_prima4:new RegExp(req.query.keyword,"i")},
-            {materia_prima5:new RegExp(req.query.keyword,"i")}, 
-        ]
-        
-    }
-    const response=await formModel.aggregate([{$match:match}])
-    res.send(response)
-    }catch(e){}
+const getFilter = async (req, res) => {
+    try {
+        let match = {}
+        const keyword = req.query.keyword
+
+        if (req.query.keyword) {
+            match.$or = [
+                { numero_producto: new RegExp(req.query.keyword,"i") },
+                { materia_prima0: new RegExp(req.query.keyword, "i") },
+                { materia_prima1: new RegExp(req.query.keyword, "i") },
+                { materia_prima2: new RegExp(req.query.keyword, "i") },
+                { materia_prima3: new RegExp(req.query.keyword, "i") },
+                { materia_prima4: new RegExp(req.query.keyword, "i") },
+                { materia_prima5: new RegExp(req.query.keyword, "i") },
+            ]
+
+        }
+        const response = await formModel.aggregate([{ $match: match }])
+        res.send(response)
+    } catch (e) { }
 }
 
-const getForm= async (req,res)=>{
-    try{
-        const listAll=await formModel.find({})
+const getForm = async (req, res) => {
+    try {
+        const listAll = await formModel.find({})
         res.send(listAll)
-    }catch(e){
-        httpError(res,e)
+    } catch (e) {
+        httpError(res, e)
     }
 }
 
-const getFormById=async (req,res)=>{
-    try{
-       const nombre_formulario=req.body
-       const listOne=await formModel.findOne({
-         nombre_formulario
+const getFormById = async (req, res) => {
+    try {
+        const nombre_formulario = req.body
+        const listOne = await formModel.findOne({
+            nombre_formulario
         })
         res.send(listOne)
-    }catch(e){
-        httpError(res,e)
+    } catch (e) {
+        httpError(res, e)
     }
 }
 
-module.exports={createForm,getForm,getFormById,getFilter}
+const updateForm = async (req, res) => {
+    try{
+    
+    var today = new Date();
+    var now = today.toLocaleString();
+    
+    const {
+        //informacion de la orden de produccion
+        nombre_formulario,
+        numero_operacion,
+        numero_producto,
+        materia_prima0,
+        materia_prima1,
+        materia_prima2,
+        materia_prima3,
+        materia_prima4,
+        materia_prima5,
+        numero_inyectora,
+        numero_molde,
+        tiempo_ciclo,
+        aditivo,
+        registrado_por,
+        //Dispositivos utilizados
+        pico_de_inyectora,
+        microgel_0,
+        microgel_1,
+        regoplas,
+        robot,
+        otro,
+        //Temperaturas del proceso
+        temp_plato_fijo,
+        temp_plato_movil,
+        temp_masa_fundida,
+        tablero_colada_caliente,
+        temp_horno,
+        //Parametros del proceso
+        tpo_ciclo_optimo,
+        tpo_ciclo_actual,
+        tpo_inyeccion,
+        tpo_extrusor,
+        tpo_enfriamiento,
+        cojin,
+        pico_presion,
+        punto_transferencia,
+        fuerza_cierre,
+        tamano_dosific,
+        contrapresion,
+        presion_empaque,
+        tpo_retencion,
+        tipo_expulsion,
+        //Cavidades habilitadas en produccion
+        cavidades,
+        observaciones
+    } = req.body
+    
+    update = await formModel.findByIdAndUpdate({
+        //informacion de la orden de produccion
+        nombre_formulario,
+        numero_operacion,
+        numero_producto,
+        materia_prima0,
+        materia_prima1,
+        materia_prima2,
+        materia_prima3,
+        materia_prima4,
+        materia_prima5,
+        numero_inyectora,
+        numero_molde,
+        tiempo_ciclo,
+        aditivo,
+        registrado_por,
+        //Dispositivos utilizados
+        pico_de_inyectora,
+        microgel_0,
+        microgel_1,
+        regoplas,
+        robot,
+        otro,
+        //Temperaturas del proceso
+        temp_plato_fijo,
+        temp_plato_movil,
+        temp_masa_fundida,
+        tablero_colada_caliente,
+        temp_horno,
+        //Parametros del proceso
+        tpo_ciclo_optimo,
+        tpo_ciclo_actual,
+        tpo_inyeccion,
+        tpo_extrusor,
+        tpo_enfriamiento,
+        cojin,
+        pico_presion,
+        punto_transferencia,
+        fuerza_cierre,
+        tamano_dosific,
+        contrapresion,
+        presion_empaque,
+        tpo_retencion,
+        tipo_expulsion,
+        //Cavidades habilitadas en produccion
+        cavidades,
+        observaciones
+    })
+    
+}catch(e){
+        httpError(res, e)
+    }
+    
+}
+
+const deleteForm= async (req,res)=>{
+    try{
+        const id=req.params
+        const deleted = await formModel.findByIdAndDelete(id)
+        res.send('Formulario Eliminado con exito!')
+    }catch(e){
+        httpError(res, e)
+    }
+}
+module.exports = { createForm, getForm, getFormById, getFilter, updateForm }
